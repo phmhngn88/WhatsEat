bc28c477e3dc4a5879faa839501e805b06a501ec
tranthivi <Tranthivi2k@gmail.com>
2022-03-21 16:15:02 +0700

    add api recipe

---
+++ whatseat-server/Models/DTOs/Requests/AddRecipeRequest.cs
@@ -0,0 +1,12 @@
+namespace whatseat_server.Models.DTOs.Requests
+{
+    public class AddRecipeRequest
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Serving { get; set; }
+        public int TotalTime { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public string RecipeTypeId { get; set; }
+    }
+}
