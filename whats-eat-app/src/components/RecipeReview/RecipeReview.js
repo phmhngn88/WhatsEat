72da9c462523c1df7a118fadcffa87d23726d0d3
NhatHiep-isme <55236520+NhatHiep-isme@users.noreply.github.com>
2022-04-12 19:36:47 +0700

    feat: review recipe

--- whats-eat-app/src/components/RecipeReview/RecipeReview.js
+++ whats-eat-app/src/components/RecipeReview/RecipeReview.js
@@ -1,25 +1,82 @@
 import React, { useEffect, useState } from "react";
 import "./RecipeReview.css";
-
+import StarRatings from "react-star-ratings";
 import axios from "axios";
 
 const RecipeReview = ({ recipeId }) => {
-  const [reviews, setReviews] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [pageSize, setPageSize] = useState(8);
+  // const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
     axios({
       method: "GET",
-      url: "https://localhost:7029/api/Recipe/reviews",
+      url: `https://localhost:7029/api/Recipe/reviews?RecipeId=${recipeId}&PageNumber=${pageNumber}&PageSize=${pageSize}`,
     })
       .then((res) => {
-        setReviews(res.data);
+        console.log(res.data);
+        // setReviews(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  return <div>Recipe review</div>;
+  return (
+    <div className="recipe-reviews-container">
+      <h1>Khám phá bình luận của người khác</h1>
+      {reviews.map((review, idx) => {
+        const { rating, comment, customer } = review;
+        return (
+          <div key={idx} className="single-review">
+            <img className="avatar-img" src={customer.avatarUrl} alt="avatar" />
+            <div className="review-detail">
+              <p className="username">{customer.name}</p>
+              <StarRatings
+                rating={rating}
+                starRatedColor="brown"
+                numberOfStars={5}
+                name="rating"
+                starDimension="25px"
+                starSpacing="3px"
+              />
+              <p className="review-content">{comment}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default RecipeReview;
+
+const reviews = [
+  {
+    rating: 3,
+    comment: "Ngon qua!",
+    customer: {
+      name: "NhatHiepisme",
+      avatarUrl:
+        "https://image.cooky.vn/recipe/g6/50880/s240x240/cooky-recipe-637102378373968618.png",
+    },
+  },
+  {
+    rating: 5,
+    comment: "Xung dang 5 sao",
+    customer: {
+      name: "NhatHiephehe",
+      avatarUrl:
+        "https://image.cooky.vn/recipe/g6/50880/s240x240/cooky-recipe-637102378373968618.png",
+    },
+  },
+  {
+    rating: 4,
+    comment: "Rat de nau!",
+    customer: {
+      name: "NhatHiepsadboiz",
+      avatarUrl:
+        "https://image.cooky.vn/recipe/g6/50880/s240x240/cooky-recipe-637102378373968618.png",
+    },
+  },
+];
