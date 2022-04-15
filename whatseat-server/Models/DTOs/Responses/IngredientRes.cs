520ba4fc5238ac8baeea3f77ae37dc6f0913ad97
Phạm Hoàng An <anphamimpala@gmail.com>
2022-04-15 20:38:40 +0700

    fix: recipe detail

--- whatseat-server/Models/DTOs/Responses/IngredientRes.cs
+++ whatseat-server/Models/DTOs/Responses/IngredientRes.cs
@@ -4,5 +4,5 @@ public class IngredientRes
 {
     public Unit unit { get; set; }
     public string name { get; set; }
-    public int quantity { get; set; }
+    public string quantity { get; set; }
 }
