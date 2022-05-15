1291ab1d0a4d53aa81aa54687bfa8bc791600a68
HaoNguyen <vanhao0643@gmail.com>
2022-05-15 22:09:19 +0700

    add migration to customer and fix appsettings

--- whatseat-server/Models/Customer.cs
+++ whatseat-server/Models/Customer.cs
@@ -12,6 +12,7 @@ public class Customer
     public string Email { get; set; }
     public string IDCard { get; set; }
     public string AvatarUrl { get; set; }
+    public float KcalPerDay { get; set; }
     [JsonIgnore]
     public ShippingInfo DefaultShippingInfo { get; set; }
     [JsonIgnore]
