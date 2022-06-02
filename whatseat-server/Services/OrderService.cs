af32187d55d7d1b2562b875d1ffc9092228bb382
Phạm Hoàng An <anphamimpala@gmail.com>
2022-06-02 04:33:19 +0100

    add order status

--- whatseat-server/Services/OrderService.cs
+++ whatseat-server/Services/OrderService.cs
@@ -32,7 +32,8 @@ public class OrderService
 
   public async Task<PagedList<Order>> GetUserPagedOrders(Customer customer, OrderPagedRequest request)
   {
-    var orders = _context.Orders.AsNoTracking().Include(o => o.Customer).Include(o => o.PaymentMethod).Include(o => o.ShippingInfo)
+    var orders = _context.Orders.AsNoTracking().Include(o => o.Customer).Include(o => o.PaymentMethod)
+    .Include(o => o.ShippingInfo).Include(o => o.OrderStatusHistories)
     .Include(o => o.Store).Include(o => o.OrderDetails).Where(o => (o.Customer == customer));
 
     var orderList = await PagedList<Order>.ToPagedList(orders, request.PageNumber, request.PageSize);
